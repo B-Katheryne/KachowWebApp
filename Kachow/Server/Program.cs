ebe90dc5ceb6b7fde81d40634b77569740124d83
Екатерина Бирюкова <m2005872@edu.misis.ru>
2022-06-08 13:04:51 +0300

    ignoring loops commit

--- Kachow/Server/Program.cs
+++ Kachow/Server/Program.cs
@@ -33,6 +33,9 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
 
 //app.UseHttpsRedirection();
 
